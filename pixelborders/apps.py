ec3b4c78eb922052e17e6bca9b7a8d3917ac7ff8
Bruce Kroeze <bkroeze@gmail.com>
2026-05-14 12:46:22 -0700

    moves pixelborders app to external repo, updates about

--- pixelborders/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PixelbordersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "pixelborders"
-    verbose_name = "Pixel Borders"
